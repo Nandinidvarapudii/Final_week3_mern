f1031fa833d73a3ca623aa03fd2ae1fcfc01126e
Nandinidvarapudii <nandininaiduu@gmail.com>
2025-01-06 18:32:53 +0530

    Add files via upload

---
+++ DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+        The terms e-commerce and e-business are often used interchangeably. The term e-tail is also sometimes used in reference to the transactional processes that make up online retail shopping.
+        </p>
+        <p>
+        E-commerce typically uses the web for at least a part of a transaction's life cycle although it may also use other technologies such as e-mail. Typical e-commerce transactions include the purchase of products 
+        </p>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
